247527eaaf43ce9306e3acfb9cc394bfb34ae3ed
Vihanga Nirmitha <vihanganirmitha200@gmail.com>
2023-08-13 11:03:34 +0530

    Algo-7 modified

--- Algo_7.java
+++ Algo_7.java
@@ -6,41 +6,21 @@ public class Algo_7 {
         do{
             
             System.out.print("\nEnter a text : ");
-            String inputsen = scanner.nextLine();
-            if(inputsen.equals("")){
+            String[] inputsen = scanner.nextLine().split(" ");
+            if(inputsen.length==0){
                 System.out.println("Text can't be empty!!!!");continue;
             }
             else{
-                char[] mychar  = inputsen.toCharArray();
-                int[] spaces = new int[mychar.length-inputsen.replace(" ", "").length()];
-                int j = 0;
-                if(spaces.length==0){
-                    System.out.print(inputsen);continue;
+                String smallest = inputsen[0];
+                String largest = inputsen[0];
+        
+                for(int i =0;i<inputsen.length;i++)
+                {
+                    smallest = (inputsen[i].length()< smallest.length())? inputsen[i]: smallest;
+                    largest = (inputsen[i].length()>largest.length())? inputsen[i]: largest;
+        
                 }
-                for(int i =0;i<mychar.length;i++){
-                    if(mychar[i] == ' '){
-                        spaces[j] = i;
-                        j++;
-                    }      
-                }
-                String largest = inputsen.substring(0, spaces[0]);
-                String smallest = inputsen.substring(0, spaces[0]);
-                for(int i =1;i<j-1;i++){
-
-                    if(i==j-1){
-                    largest = (largest.length()<(inputsen.substring(spaces[i]+1)).length())? (inputsen.substring(spaces[i]+1)): largest;
-                    smallest = (smallest.length()>(inputsen.substring(spaces[i]+1)).length())? (inputsen.substring(spaces[i]+1)): smallest;
-                    }
-                    else{
-                    largest = (largest.length()<(inputsen.substring(spaces[i-1]+1, spaces[i]-1)).length())? (inputsen.substring(spaces[i-1]+1, spaces[i])): largest;
-                    smallest = (smallest.length()>(inputsen.substring(spaces[i-1]+1, spaces[i]-1)).length())? (inputsen.substring(spaces[i-1]+1, spaces[i])): smallest;
-                    }
-                   //if(i==0)System.out.print(inputsen.substring(spaces[j-i-1]+1));
-                   // if (i==j-1)System.out.print(" "+inputsen.substring(0, spaces[j-i-1]) );
-                    //else System.out.print(inputsen.substring(spaces[j-i-2], spaces[j-i-1]) );
-                }
-                System.out.println("Smallest word : "+ smallest+"_");
-                System.out.println("largest word : "+ largest+"_");
+                System.out.printf("Smallest number : %s\nLargest number : %s\n",smallest,largest);
             }
 
 
